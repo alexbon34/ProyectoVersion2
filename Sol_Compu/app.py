24ba01cb64c7a2a6a0c6c325504cb7033ee5417a
Nico230202 <101598135+Nico230202@users.noreply.github.com>
2024-10-26 01:10:47 -0600

    Add files via upload
    Ya viene todo lo de la carga de datos con las relaciones necesarias para las consultas.

--- Sol_Compu/app.py
+++ Sol_Compu/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, get_flashed_messages, jsonify, render_template, redirect, url_for, request, flash
 from conexionBD import conectar_bd 
-from py2neo import Graph, Node 
+from py2neo import Graph, Node, Relationship
 import os
 import csv
 
@@ -41,15 +41,6 @@ def borrar_base_datos():
 
     return redirect(url_for('carga_datos'))
 
-@app.route('/cargar_relaciones', methods=['POST'])
-def cargar_relaciones():
-    try:
-        #relacionar los Creadores con 
-        pass
-    except Exception as e:
-        flash(f'ERROR <Relaciones no creadas>')
-
-
 #Cargas archivo GEMINI_API_COMPETITION
 @app.route('/cargar_Gemini_API', methods=['POST'])
 def cargar_Gemini_API():
@@ -70,31 +61,28 @@ def cargar_Gemini_API():
         return redirect(url_for('carga_datos'))
 
     try:
+        #aqui creo los noditos de tecnologia
+        # Aquí creo los noditos de tecnología
         with open(file_path, newline='', encoding='utf-8') as csvfile:            
             reader = csv.DictReader(csvfile)
             for row in reader:
                 built_with_raw = row.get('Built With', '') 
                 tecnologias_list = [tech.strip() for tech in built_with_raw.split(',')]  # Crear lista de tecnologías sin espacios extra
-                print(f"Tecnologías procesadas: {tecnologias_list}")
-
+                By = row.get('By', '').strip()  # Obtener el creador de la fila
                 for tecnologia in tecnologias_list:
                     if tecnologia:  # Asegurarse de que no sea una cadena vacía
-                        # Verificar si el nodo ya existe para esta tecnología individual
                         existe_tecnologia = graph.evaluate("MATCH (t:Tecnologias {Built_With: $tecnologia}) RETURN t", tecnologia=tecnologia)
-                        print(f"Verificando tecnología: '{tecnologia}' - Existe: {'Sí' if existe_tecnologia else 'No'}")
-
                         if not existe_tecnologia:
                             try:
-                                # Crear nodo de tecnología individual
-                                tecnologias = Node("Tecnologias", Built_With=tecnologia)
+                                # Crear el nodo de tecnología con el campo 'By'
+                                tecnologias = Node("Tecnologias", Built_With=tecnologia, By=By)
                                 graph.create(tecnologias)
-                                print(f"Tecnología creada: {tecnologia}")
                             except Exception as e:
                                 flash(f'Error al crear nodo de Tecnologías: {e}', 'danger')
                         else:
-                            print(f"La tecnología '{tecnologia}' ya existe. No se creará un duplicado.")                         
-            
-        #vuelvo abrir el archivo       
+                            print(f"La tecnología '{tecnologia}' ya existe. No se creará un duplicado.")
+
+        #vuelvo abrir el archivo y creo los noditos de aplicacion      
         with open(file_path, newline='', encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile)      
             #creo el nodito de Aplicaciones            
@@ -117,26 +105,31 @@ def cargar_Gemini_API():
                         else:
                             print(f"El nodo de Aplicaciones con Title '{Title}' ya existe. No se creará un duplicado.")
 
-
-        #vuelvo abror el archivo para procesarlo.                 
+        # Procesando Creadores
         with open(file_path, newline='', encoding='utf-8') as csvfile:
-            print("Procesando Creadores")
-            reader = csv.DictReader(csvfile)
-            #creo el nodito de Creadores
-            for row in reader:
-                Title = row.get('Title', '')
-                By = row.get('By', '')
-                Location = row.get('Location', '')
+           print("Procesando Creadores")
+           reader = csv.DictReader(csvfile)
 
-                print(f"Procesando Creadores - Title: {Title}, By: {By}")
-                if not graph.evaluate(f"MATCH (c:Creadores {{By: '{By}'}}) RETURN c"):
-                    try:
-                        creadores = Node("Creadores", Title=Title, By=By, Location=Location)
-                        graph.create(creadores)
-                    except Exception as e:
-                        flash(f'Error al crear nodo de Creadores: {e}', 'danger')
-                else:
-                    print(f"El nodo con By '{By}' ya existe. No se creará un duplicado.")
+           for row in reader:
+               Title = row.get('Title', '')
+               By = row.get('By', '')  # Obtener el campo de creadores
+               Location = row.get('Location', '')
+
+               # Separar los creadores por comas
+               if By:
+                   creadores = [creator.strip() for creator in By.split(',')]  # Lista de creadores
+
+                   for creator in creadores:
+                       if creator:  # Asegúrate de que no esté vacío
+                           print(f"Procesando Creador - Title: {Title}, By: {creator}")
+                           if not graph.evaluate(f"MATCH (c:Creadores {{By: '{creator}'}}) RETURN c"):
+                               try:
+                                   creadores_node = Node("Creadores", Title=Title, By=creator, Location=Location)
+                                   graph.create(creadores_node)
+                               except Exception as e:
+                                   flash(f'Error al crear nodo de Creadores: {e}', 'danger')
+                       else:
+                           print(f"Creador vacío encontrado para la aplicación '{Title}'.")
 
         #esto es escencial para simplemente crear los noditos de la region
         with open(file_path, newline='', encoding='utf-8') as csvfile:
@@ -161,8 +154,125 @@ def cargar_Gemini_API():
                         else:
                             print(f"El nodo de Regiones con Location '{locs}' ya existe. No se creará un duplicado.")
     
-    
-            
+        # Crear relaciones entre Creadores y Regiones
+        with open(file_path, newline='', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                By = row.get('By', '').strip()
+                Location = row.get('Location', '').strip()
+                if not By or not Location:  # Verificar que tanto el creador como la ubicación no estén vacíos
+                    continue
+                location_list = [loc.strip() for loc in Location.split(',')]
+                creadores_list = [creator.strip() for creator in By.split(',')] if By else []
+                for creador_name in creadores_list:
+                    creador = graph.evaluate("MATCH (c:Creadores {By: $by}) RETURN c", by=creador_name)
+                    if creador:
+                        for loc in location_list:
+                            region = graph.evaluate("MATCH (r:Regiones {Location: $loc}) RETURN r", loc=loc)
+                            if region:
+                                if not graph.evaluate("""
+                                    MATCH (c:Creadores {By: $by})-[rel:LOCATED_IN]->(r:Regiones {Location: $loc}) RETURN rel
+                                """, by=creador_name, loc=loc):
+                                    graph.create(Relationship(creador, "LOCATED_IN", region))
+                                    print(f"Relación 'LOCATED_IN' creada entre el creador '{creador_name}' y la región '{loc}'.")
+                            else:
+                                print(f"No se encontró la región para '{loc}', por lo que no se puede crear la relación.")
+                    else:
+                        print(f"No se encontró el creador '{creador_name}'.")
+        
+        # Crear relación entre Aplicaciones y Creadores
+        with open(file_path, newline='', encoding='utf-8') as csvfile:
+            print("Creando relaciones entre Aplicaciones y Creadores")
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                Title = row.get('Title', '')
+                By = row.get('By', '')
+                creadores_list = [creator.strip() for creator in By.split(',')] if By else [None]
+
+                for creator in creadores_list:
+                    if creator is None:
+                        print(f"No se ha proporcionado creador para la aplicación '{Title}'. No se creará relación.")
+                    else:
+                        aplicacion = graph.evaluate("MATCH (a:Aplicaciones {Title: $Title}) RETURN a", Title=Title)
+                        creador_node = graph.evaluate("MATCH (c:Creadores {By: $creator}) RETURN c", creator=creator)
+                        if aplicacion and creador_node:
+                            if not graph.evaluate("""
+                                MATCH (a:Aplicaciones {Title: $Title})-[rel:TIENE]->(c:Creadores {By: $creator}) RETURN rel
+                            """, Title=Title, creator=creator):
+                                graph.create(Relationship(aplicacion, "TIENE", creador_node))
+                                print(f"Relación 'TIENE' creada entre la aplicación '{Title}' y el creador '{creator}'.")
+                        else:
+                            if not aplicacion:
+                                print(f"No se encontró la aplicación '{Title}'.")
+                            if not creador_node:
+                                print(f"No se encontró el creador '{creator}'.")
+
+
+        # Crear relaciones entre Creadores y Tecnologías
+        with open(file_path, newline='', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                built_with_raw = row.get('Built With', '').strip()  # Obtener la columna "Built With"
+                By = row.get('By', '').strip()  # Obtener el nombre del creador
+                # Crear una lista de creadores eliminando espacios en blanco
+                creadores_list = [creator.strip() for creator in By.split(',')] if By else []
+
+                # Crear una lista de tecnologías eliminando espacios en blanco
+                tecnologias_list = [tech.strip() for tech in built_with_raw.split(',')] if built_with_raw else []
+
+                # Iterar sobre la lista de tecnologías
+                for tecnologia in tecnologias_list:
+                    if tecnologia:  # Asegurarse de que no sea una cadena vacía
+                        tecnologia_node = graph.evaluate("MATCH (t:Tecnologias {Built_With: $tecnologia}) RETURN t", tecnologia=tecnologia)
+
+                        # Verificar si la tecnología existe
+                        if tecnologia_node:
+                            # Iterar sobre los creadores y crear relaciones
+                            for creador_name in creadores_list:
+                                creador = graph.evaluate("MATCH (c:Creadores {By: $by}) RETURN c", by=creador_name)
+
+                                if creador:
+                                    # Verificar que la relación no exista
+                                    if not graph.evaluate("""
+                                        MATCH (c:Creadores {By: $by})-[rel:TRABAJA_CON]->(t:Tecnologias {Built_With: $tecnologia}) 
+                                        RETURN rel
+                                    """, by=creador_name, tecnologia=tecnologia):
+                                        # Crear la relación TRABAJA_CON
+                                        graph.create(Relationship(creador, "TRABAJA_CON", tecnologia_node))
+                                        print(f"Relación 'TRABAJA_CON' creada entre el creador '{creador_name}' y la tecnología '{tecnologia}'.")
+                                else:
+                                    print(f"No se encontró el creador '{creador_name}'.")
+                        else:
+                            print(f"No se encontró la tecnología '{tecnologia}'.")
+
+        # Crear relaciones entre Aplicaciones y Tecnologías
+        with open(file_path, newline='', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                Title = row.get('Title', '').strip()
+                built_with_raw = row.get('Built With', '').strip()
+                tecnologias_list = [tech.strip() for tech in built_with_raw.split(',')] if built_with_raw else []
+
+                aplicacion = graph.evaluate("MATCH (a:Aplicaciones {Title: $title}) RETURN a", title=Title)
+
+                for tecnologia in tecnologias_list:
+                    if tecnologia:
+                        tecnologia_node = graph.evaluate("MATCH (t:Tecnologias {Built_With: $tecnologia}) RETURN t", tecnologia=tecnologia)
+                        if aplicacion and tecnologia_node:
+                            if not graph.evaluate("""
+                                MATCH (a:Aplicaciones {Title: $title})-[rel:UTILIZA]->(t:Tecnologias {Built_With: $tecnologia}) RETURN rel
+                            """, title=Title, tecnologia=tecnologia):
+                                graph.create(Relationship(aplicacion, "UTILIZA", tecnologia_node))
+                                print(f"Relación 'UTILIZA' creada entre la aplicación '{Title}' y la tecnología '{tecnologia}'.")
+                        else:
+                            if not aplicacion:
+                                print(f"No se encontró la aplicación '{Title}'.")
+                            if not tecnologia_node:
+                                print(f"No se encontró la tecnología '{tecnologia}'.")
+
+
+
+
         flash('Nodos cargados exitosamente en la base de datos.', 'success')
     except Exception as e:
         print("Creo que entro aqui al except")
